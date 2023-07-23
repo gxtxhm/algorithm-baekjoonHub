525cda8deaefb40ea7b4bbbc9cb06069371f819f
gxtxhm <53137918+gxtxhm@users.noreply.github.com>
2023-07-23 11:41:08 +0900

    [unrated] Title: 덧셈식 출력하기, Time: 2.23 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181947. 덧셈식 출력하기/덧셈식 출력하기.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+    cout << a;
+    cout<<" + ";
+    cout<<b;
+    cout<<" = ";
+    cout<<a+b;
+    return 0;
+}
