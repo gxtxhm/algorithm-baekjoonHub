7f9d27c628a9403c7f03e66d65c89dca20bfac61
gxtxhm <53137918+gxtxhm@users.noreply.github.com>
2024-07-18 15:42:27 +0900

    [level 1] Title: 소수 찾기, Time: 2.53 ms, Memory: 3.86 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+     vector<bool> arr(n+1,true);
+    
+    for(int i=2;i*i<=n;i++)
+    {
+        if(arr[i])
+        {
+            for(int j=i*i;j<=n;j+=i)
+                arr[j]=false;
+        }
+    }
+    
+    for(int i=2;i<=n;i++)
+        if(arr[i])
+            answer++;
+    return answer;
+    
+    return answer;
+}
