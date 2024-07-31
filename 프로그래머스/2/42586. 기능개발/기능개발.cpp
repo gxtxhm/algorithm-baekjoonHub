cfeb78f5fe6bf42a4b79a7f80bffdbee5fb67823
gxtxhm <53137918+gxtxhm@users.noreply.github.com>
2024-07-31 13:21:28 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 4.04 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -5,33 +5,27 @@ using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    int num=0;
-    int size = progresses.size();
-    while(num<size)
+    
+    int max_day=0;
+    
+    for(int i=0;i<progresses.size();i++)
     {
-        if(progresses[num]<100)
-        {
-            for(int i=num;i<size;i++)
-                progresses[i]+=speeds[i];
-        }
+        // 100 일 때는 나머지가 0인지 아닌지에 따라 나누지만 99로 하면 무조건 +1을
+        // 해야함
+        // day는 현재 작업이 완료되기까지의 일 수
+        int day = (99-progresses[i]) / speeds[i] + 1;    
+        
+        // 빈 경우는 맨처음, max_day는 지금 이 작업이 배포되려면 며칠을 기다려야하는지
+        if(answer.empty() || max_day < day)
+            answer.push_back(1);
         else
-        {
-            int cnt=0;
-            for(int i=num;i<size;i++)
-            {
-                if(progresses[i]>=100)
-                {
-                    num++;
-                    cnt++;
-                }
-                else
-                {
-                    num=i;
-                    break;
-                }
-            }
-            answer.push_back(cnt);
-        }
+        // max_day보다 day가 작으면 이미 작업이 완료된 상태이기 때문에 배포수 + 1
+            answer.back()++;
+        
+        // day가 더 크면 이전까지의 작업은 다 배포완료되었고 지금 i번째 작업이 완료되기를
+        // 기다려야한다.
+        if(max_day<day)
+            max_day = day;
     }
     
     return answer;
