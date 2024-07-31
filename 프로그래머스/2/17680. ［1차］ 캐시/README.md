7b97cdcb3e98b631994c38642af637a260c723b0
gxtxhm <53137918+gxtxhm@users.noreply.github.com>
2024-07-31 19:25:30 +0900

    [level 2] Title: [1차] 캐시, Time: 21.45 ms, Memory: 12.5 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/README.md
+++ 프로그래머스/2/17680. ［1차］ 캐시/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 캐시 - 17680 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680) 
 
 ### 성능 요약
 
-메모리: 12.4 MB, 시간: 50.48 ms
+메모리: 12.5 MB, 시간: 21.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 31일 16:55:03
+2024년 07월 31일 19:25:27
 
 ### 문제 설명
 
--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.cpp
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.cpp
@@ -1,68 +1,41 @@
 #include <string>
 #include <vector>
-#include <map>
+#include <list>
+#include <unordered_map>
 #include <algorithm>
+
 using namespace std;
 
 int solution(int cacheSize, vector<string> cities) {
-    int answer = 0;
-
-    map<string, int> m;
-    vector<pair<int, string>> cacheList;
-
-    if (cacheSize == 0)
+    if (cacheSize == 0) {
         return cities.size() * 5;
-
-    for (int i = 0; i < cities.size(); i++)
-        transform(cities[i].begin(), cities[i].end(), cities[i].begin(), ::tolower);
-    int i;
-    for (i = 0; m.size()<cacheSize; i++)
-    {
-        if (m.find(cities[i]) != m.end())
-        {
-            answer += 1;
-            m[cities[i]] = i;
-            for (int j = 0; j <cacheList.size(); j++)
-                if (cacheList[j].second == cities[i])
-                    cacheList[j].first = i;
-        }
-        else
-        {
-            answer += 5;
-            m.insert({ cities[i],i });
-            cacheList.push_back({ i,cities[i] });
-        }
-
     }
 
-    for (; i < cities.size(); i++)
-    {
-        // 캐시 히트
-        if (m.find(cities[i]) != m.end())
-        {
-            answer += 1;
-            m[cities[i]] = i;
-            for (int j = 0; j < cacheSize; j++)
-                if (cacheList[j].second == cities[i])
-                    cacheList[j].first = i;
-        }
-        // 캐시 미스
-        else
-        {
-            answer += 5;
-            int minId = 0;
-            // 가장오래된거 빼고 새로 넣어야함
-            for (int j = 1; j < cacheSize; j++)
-            {
-                if (cacheList[minId].first > cacheList[j].first)
-                    minId = j;
+    list<string> cache;
+    unordered_map<string, list<string>::iterator> cache_map;
+    int time = 0;
+
+    for (auto& city : cities) {
+        transform(city.begin(), city.end(), city.begin(), ::tolower);
+        if (cache_map.find(city) != cache_map.end()) {
+            // Cache hit
+            time += 1;
+            cache.erase(cache_map[city]);
+            cache.push_front(city);
+            cache_map[city] = cache.begin();
+        } else {
+            // Cache miss
+            time += 5;
+            if (cache.size() == cacheSize) {
+                // Remove the least recently used item
+                cache_map.erase(cache.back());
+                cache.pop_back();
             }
-            m.erase(cacheList[minId].second);
-            cacheList[minId] = make_pair(i, cities[i]);
-
-            m.insert({ cities[i],i });
+            // Add the new city to the cache
+            cache.push_front(city);
+            cache_map[city] = cache.begin();
         }
     }
 
-    return answer;
+    return time;
 }
