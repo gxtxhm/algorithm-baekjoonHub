cc9e77cb64c068a92daefdb05d312b0d77b3eb43
gxtxhm <53137918+gxtxhm@users.noreply.github.com>
2024-08-01 19:02:50 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.13 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
@@ -3,73 +3,47 @@
 #include <unordered_map>
 using namespace std;
 
+unordered_map<string,int> make_map(const string& str)
+{
+    unordered_map<string,int> um;
+    
+    for(int i=0;i<str.length()-1;i++)
+    {
+        if(isalpha(str[i])&&isalpha(str[i+1]))
+        {
+            string s = {(char)tolower(str[i]),(char)tolower(str[i+1])};
+            um[s]++;
+        }
+    }
+    return um;
+}
+
 int solution(string str1, string str2) {
-    int answer = 0;
 
-    unordered_map<string, int> um1;
-    unordered_map<string, int> um2;
-
-    for (int i = 0; i < str1.length() - 1; i++)
+    unordered_map<string,int> um1 = make_map(str1);
+    unordered_map<string,int> um2 = make_map(str2);    
+    
+    int intersectionSize=0;
+    int unionSize=0;
+    
+    for(auto& item : um1)
     {
-        string s;
-        s += str1[i];
-        s += str1[i + 1];
-        if (str1[i] == ' ' || str1[i + 1] == ' ')continue;
-        if (isalpha(str1[i]) && isalpha(str1[i + 1]))
+        auto it = um2.find(item.first);
+        // 찾았다면 
+        if(it!=um2.end())
         {
-            s[0] = tolower(s[0]);
-            s[1] = tolower(s[1]);
-
-            if (um1.find(s) != um1.end())
-                um1[s]++;
-            else
-                um1.insert({ s,1 });
-        }
-    }
-    for (int i = 0; i < str2.length() - 1; i++)
-    {
-        string s;
-        s += str2[i];
-        s += str2[i + 1];
-        if (isalpha(str2[i])&& isalpha(str2[i + 1]))
-        {
-            s[0] = tolower(s[0]);
-            s[1] = tolower(s[1]);
-
-            if (um2.find(s) != um2.end())
-                um2[s]++;
-            else
-                um2.insert({ s,1 });
-        }
-    }
-    int a = 0, b = 0;
-    unordered_map<string, int> temp;
-    if (um1.size() > um2.size())
-    {
-        temp = um1;
-        um1 = um2;
-        um2 = temp;
-    }
-    for (auto& item : um1)
-    {
-        if (um2.find(item.first) != um2.end())
-        {
-            a += min(item.second, um2[item.first]);
+            intersectionSize += min(item.second,um2[item.first]);
+            unionSize += max(item.second,um2[item.first]);
+            um2.erase(it);
         }
         else
-            b += item.second;
-
+            unionSize += item.second;
     }
-    for (auto& item : um2)
+    
+    for(const auto& item : um2)
     {
-        if (um1.find(item.first) != um1.end())
-        {
-            b += max(item.second, um1[item.first]);
-        }
-        else
-            b += item.second;
+        unionSize+=item.second;
     }
-    if (a == 0 && b == 0)return 65536;
-    answer = (int)(((float)a / b) * 65536);
-    return answer;
+    if(unionSize==0)return 65536;
+    return static_cast<int>(static_cast<double>(intersectionSize)/unionSize*65536);
 }
