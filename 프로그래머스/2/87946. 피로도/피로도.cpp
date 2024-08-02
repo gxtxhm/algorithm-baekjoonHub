f303e1de9afe368d2816552481a281d9c6ba7458
gxtxhm <53137918+gxtxhm@users.noreply.github.com>
2024-08-02 15:03:23 +0900

    [level 2] Title: 피로도, Time: 1.17 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int maxCnt=-1;
+vector<bool> ch(8,0);
+int enterCnt=0;
+
+void recur(int cnt,int curk,const vector<vector<int>>& dungeons)
+{
+    if(cnt==dungeons.size())
+    {
+        if(enterCnt>maxCnt)
+            maxCnt=enterCnt;
+        return;
+    }
+    for(int i=0;i<dungeons.size();i++)
+    {
+        if(ch[i]==false&&curk>=dungeons[i][0])
+        {
+            enterCnt++;
+            ch[i]=true;
+            recur(cnt+1,curk-dungeons[i][1],dungeons);
+            ch[i]=false;enterCnt--;
+        }
+    }
+    if(enterCnt>maxCnt)
+            maxCnt=enterCnt;
+}
+
+int solution(int k, vector<vector<int>> dungeons) {
+    int answer = -1;
+    recur(0,k,dungeons);
+    return maxCnt;
+}
