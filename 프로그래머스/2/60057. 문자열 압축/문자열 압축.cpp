112a5fea6ec0a6ef523363a7e401c41ac6a1612d
gxtxhm <53137918+gxtxhm@users.noreply.github.com>
2024-08-09 14:34:44 +0900

    [level 2] Title: 문자열 압축, Time: 0.84 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 1001;
+    
+    if(s.length()==1)
+        return 1;
+    
+    for(int i=1;i<=s.length()/2;i++)
+    {
+        string temp = "";
+        int j;
+        for(j=0;j<=s.length()-i;j+=i)
+        {
+            int cnt=1;
+            while(j+i<=s.length()-1 && s.substr(j,i)==s.substr(j+i,i))
+            {
+                cnt++; j+=i;
+            }
+            if(cnt>1)
+            {
+                temp += to_string(cnt) + s.substr(j-i,i);
+            }
+            else
+                temp+=s.substr(j,i);
+        }
+        if(j>s.length()-i)
+            temp+=s.substr(j);
+        if(temp.length()<answer)
+            answer=temp.length();
+    }
+    
+    return answer;
+}
