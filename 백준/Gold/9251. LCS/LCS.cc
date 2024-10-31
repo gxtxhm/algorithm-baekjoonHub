7f067b13770e86f55433bef47d66788a15890434
gxtxhm <53137918+gxtxhm@users.noreply.github.com>
2024-10-31 20:16:17 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 6004 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+    string s,s2;cin>>s>>s2;
+    
+    vector<vector<int>> dp(s.length()+1,vector<int>(s2.length()+1,0));
+    
+    for(int i=1;i<=s.length();i++)
+    {
+        for(int j=1;j<=s2.length();j++)
+        {
+            if(s[i-1]==s2[j-1])dp[i][j]=dp[i-1][j-1]+1;
+            else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        }
+    }
+    cout<<dp[s.length()][s2.length()];
+	return 0;
+}
