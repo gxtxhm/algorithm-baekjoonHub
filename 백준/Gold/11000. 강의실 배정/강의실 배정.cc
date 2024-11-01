30b653a32806c2125167750b8530570df3eeec95
gxtxhm <53137918+gxtxhm@users.noreply.github.com>
2024-11-01 17:33:12 +0900

    [Gold V] Title: 강의실 배정, Time: 48 ms, Memory: 4484 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.cc
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.cc
@@ -1,31 +1,29 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
+
+
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
-    int n;
-    cin >> n;
-    vector<pair<int, int>> lectures(n);
-    for (int i = 0; i < n; i++) {
-        cin >> lectures[i].first >> lectures[i].second;
-    }
-
-    // 시작 시간을 기준으로 정렬
-    sort(lectures.begin(), lectures.end());
-
-    // 종료 시간을 저장하는 최소 힙
+    cout.tie(NULL);
+    int n; cin >> n;
     priority_queue<int, vector<int>, greater<int>> pq;
-    pq.push(lectures[0].second);
-
-    for (int i = 1; i < n; i++) {
-        if (pq.top() <= lectures[i].first) {
-            pq.pop();  // 기존 강의실을 사용할 수 있으므로 제거
-        }
-        pq.push(lectures[i].second);  // 새 강의 종료 시간 추가
+    vector<pair<int, int>> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i].first >> v[i].second;
     }
+    sort(v.begin(), v.end());
 
-    cout << pq.size();  // 필요한 강의실 개수
+    pq.push(v[0].second);
+
+    for (int i = 1; i < n; i++)
+    {
+        if (v[i].first >= pq.top())pq.pop();
+        pq.push(v[i].second);
+    }
+    cout << pq.size();
     return 0;
 }
