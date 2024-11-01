58c5d4a91ba664a969a2f19ad983081575b2053d
gxtxhm <53137918+gxtxhm@users.noreply.github.com>
2024-11-01 13:29:29 +0900

    [Silver II] Title: 최대 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;cin>>n;
+    priority_queue<int,vector<int>,less<int>> pq;
+    for(int i=0;i<n;i++)
+    {
+        int a;cin>>a;
+        if(a==0)
+        {
+            if(pq.empty())cout<<0<<'\n';
+            else {
+                cout<<pq.top()<<'\n';pq.pop();
+            }
+        }
+        else pq.push(a);
+    }
+    
+	return 0;
+}
